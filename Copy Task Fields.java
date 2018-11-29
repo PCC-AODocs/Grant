9bd5044a47c7a5c73f1a3f91478a5cfa780b7f1c
arletteslachmuylder-pcc <arlette.slachmuylder@pcc.edu>
2018-11-28 22:18:10 -0800

    Update Copy Task Fields.java
    Made Agency a single drop down and added Agency/Competition

--- Copy Task Fields.java
+++ Copy Task Fields.java
@@ -2,40 +2,52 @@ import com.altirnao.aodocs.custom.*;
 import java.util.List;
 
 public void update(ReadableDocument document) throws Exception {
-	DocumentService documentService = getDocumentService();
-	Document task= documentService.lockDocument(document);
+	
+DocumentService documentService = getDocumentService();
+Document task= documentService.lockDocument(document);
 
-	//---------------------------------------
-	//TEST 1 - does Task have a parent Grant?
-	//---------------------------------------
-	List grantIds = task.getFromRelatedDocumentIds("Grant to Task");
+//---------------------------------------
+//TEST 1 - does Task have a parent Grant?
+//---------------------------------------
+List grantIds = task.getFromRelatedDocumentIds("Grant to Task");
 
-	//----------------------------------------------------
-	//TEST 2 - does Task have parent Report?
-	//----------------------------------------------------
+//----------------------------------------------------
+//TEST 2 - does Task have parent Report?
+//----------------------------------------------------
 
-	List reportIds = task.getFromRelatedDocumentIds("Report to Task");
-	List reports = documentService.loadReadableDocuments(reportIds);
-	if(!reports.isEmpty()){
-	   ReadableDocument report = (ReadableDocument)reports.get(0);
-	   task.setField("Report Title", report.getTitle());
-	   if(grantIds.size() == 0){
-		 grantIds = report.getFromRelatedDocumentIds("Grant to Report");
-	   }
-	}
+List reportIds = task.getFromRelatedDocumentIds("Report to Task");
+List reports = documentService.loadReadableDocuments(reportIds);
+if(!reports.isEmpty()){
+   ReadableDocument report = (ReadableDocument)reports.get(0);
+   task.setField("Report Title", report.getTitle());
+   if(grantIds.size() == 0){
+     grantIds = report.getFromRelatedDocumentIds("Grant to Report");
+   }
+}
 
 
-	//---------------------------------------
-	//Assign values from Grant
-	//---------------------------------------
-	List grants = documentService.loadReadableDocuments(grantIds);
-	if (!grants.isEmpty()) {
-	   ReadableDocument grant = (ReadableDocument) grants.get(0);
-	   task.addFromRelatedDocument("Grant to Task", grant);
-	   task.setField("Project Title", grant.getTitle());
-	   task.setHierarchicalCategory("Agency", grant.getHierarchicalCategory("Agency"));
-	   task.setField("Competition", grant.getField("Competition"));
-	   task.setField("PCC Director / Lead", grant.getField("PCC Director / Lead"));
-	   task.setField("Grant Id", grant.getField("Grant Id"));
-	}
+//---------------------------------------
+//Assign values from Grant
+//---------------------------------------
+List grants = documentService.loadReadableDocuments(grantIds);
+if (!grants.isEmpty()) {
+
+   ReadableDocument grant = (ReadableDocument) grants.get(0);
+
+  List<String> agencyOnly = getCategoryService().listValues("Agency");
+  String agency = (String)grant.getField("Agency");
+
+  if(!agencyOnly.contains(agency)){
+   getCategoryService().createCategoryValue("Agency", agency, "");
+  }
+
+   task.addFromRelatedDocument("Grant to Task", grant);
+   task.setField("Project Title", grant.getTitle());
+   task.setField("Agency", grant.getField("Agency"));
+   task.setHierarchicalCategory("Agency/Competition", grant.getHierarchicalCategory("Agency/Competition"));
+   task.setField("Competition", grant.getField("Competition"));
+   task.setField("PCC Director / Lead", grant.getField("PCC Director / Lead"));
+   task.setField("Grant Id", grant.getField("Grant Id"));
+}
+
 }
