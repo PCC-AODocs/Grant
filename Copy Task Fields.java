e82ee0cd296cbe36053fb2e87b4f0bd92f78f581
Arlette Slachmuuylder <arlette.slachmuylder@pcc.edu>
2018-12-14 17:02:35 -0800

    Latest changes

--- Copy Task Fields.java
+++ Copy Task Fields.java
@@ -2,45 +2,60 @@ import com.altirnao.aodocs.custom.*;
 import java.util.List;
 
 public void update(ReadableDocument document) throws Exception {
-	
-DocumentService documentService = getDocumentService();
-Document task= documentService.lockDocument(document);
 
-//---------------------------------------
-//TEST 1 - does Task have a parent Grant?
-//---------------------------------------
-List grantIds = task.getFromRelatedDocumentIds("Grant to Task");
+	DocumentService documentService = getDocumentService();
+	Document task= documentService.lockDocument(document);
 
-//----------------------------------------------------
-//TEST 2 - does Task have parent Report?
-//----------------------------------------------------
+	//---------------------------------------
+	//TEST 1 - does Task have a parent Grant?
+	//---------------------------------------
+	List grantIds = task.getFromRelatedDocumentIds("Grant to Task");
 
-List reportIds = task.getFromRelatedDocumentIds("Report to Task");
-List reports = documentService.loadReadableDocuments(reportIds);
-if(!reports.isEmpty()){
-   ReadableDocument report = (ReadableDocument)reports.get(0);
-   task.setField("Report Title", report.getTitle());
-   if(grantIds.size() == 0){
-     grantIds = report.getFromRelatedDocumentIds("Grant to Report");
-   }
-}
+	//----------------------------------------------------
+	//TEST 2 - does Task have parent Report?
+	//----------------------------------------------------
+
+	List reportIds = task.getFromRelatedDocumentIds("Report to Task");
+	List reports = documentService.loadReadableDocuments(reportIds);
+	if(!reports.isEmpty()){
+	   ReadableDocument report = (ReadableDocument)reports.get(0);
+	   task.setField("Report Title", report.getTitle());
+	   task.setField("Admin Signature", report.getField("Admin Signature"));
+	   if(grantIds.size() == 0){
+	     grantIds = report.getFromRelatedDocumentIds("Grant to Report");
+	   }
+	}
 
 
-//---------------------------------------
-//Assign values from Grant
-//---------------------------------------
-List grants = documentService.loadReadableDocuments(grantIds);
-if (!grants.isEmpty()) {
 
-   ReadableDocument grant = (ReadableDocument) grants.get(0);
 
-   task.addFromRelatedDocument("Grant to Task", grant);
-   task.setField("Project Title", grant.getTitle());
-   task.setField("Agency", grant.getField("Agency"));
-   task.setHierarchicalCategory("Agency/Competition", grant.getHierarchicalCategory("Agency/Competition"));
-   task.setField("Competition", grant.getField("Competition"));
-   task.setField("PCC Director / Lead", grant.getField("PCC Director / Lead"));
-   task.setField("Grant Id", grant.getField("Grant Id"));
-}
+	//---------------------------------------
+	//Assign values from Grant
+	//---------------------------------------
+	List grants = documentService.loadReadableDocuments(grantIds);
+	if (!grants.isEmpty()) {
+
+	   ReadableDocument grant = (ReadableDocument) grants.get(0);
+
+	  //retrieve the categories we will want to check to see if need new entries
+	  List<String> agencyOnly = getCategoryService().listValues("Agency");
+	  String agency = (String)grant.getField("Agency");
+
+	  // -------------------------------------------------------
+	  // Check if entry needed for Agency
+	  //--------------------------------------------------------
+	  if(!agencyOnly.contains(agency)){
+	   getCategoryService().createCategoryValue("Agency", agency, "");
+	  }
+
+	   task.addFromRelatedDocument("Grant to Task", grant);
+	   task.setField("Project Title", grant.getTitle());
+	   task.setField("Agency", agency);
+	   task.setHierarchicalCategory("Agency/Competition", grant.getHierarchicalCategory("Agency/Competition"));
+	   task.setField("Competition", grant.getField("Competition"));
+	   task.setField("PCC Director / Lead", grant.getField("PCC Director / Lead"));
+	   task.setField("Grant Id", grant.getField("Grant Id"));
+	}
+
 
 }
--- Set Grant Agency-Competition.java
+++ Set Grant Agency-Competition.java
@@ -6,41 +6,73 @@ import com.altirnao.aodocs.custom.*;
 
 public void update(ReadableDocument document) throws Exception {
 
-String agency = (String)document.getHierarchicalCategory("Agency").get(0);
-String competition = (String)document.getHierarchicalCategory("Competition").get(0);
-String title = (String)document.getTitle();
-Boolean createAgency = true;
-Boolean createCompetition = true;
-Boolean createTitle = true;
+  //------------------------------------------------------------------------
+  //DESCRIPTION
+  //Creates the category entries if needed, based on entries in the Grant
+  // for Agency/Competition and Agency
+  // These categories are built as Grants are added, and then used to filter
+  // items in the views
+  //
+  //USAGE: Grant
+  //-------------------------------------------------------------------------
 
-List<String> items = getCategoryService().listValues("Agency/Competition");
-List<String> agencyOnly = getCategoryService().listValues("Agency");
+  //--------------------------------------------------------------------------------------------------------
+  //Pull in the three fields from grant needed for the category: agency, competition, Title
+  // Agency is pulled from the Agency/Division category, but just pulling the first in the hierarchy, Agency
+  //--------------------------------------------------------------------------------------------------------
+  String agency = (String)document.getHierarchicalCategory("Agency").get(0);
+  String competition = (String)document.getHierarchicalCategory("Competition").get(0);
+  String title = (String)document.getTitle();
 
-if(items.contains(agency)){
-  createAgency = false;
-  List<string> competitions = getCategoryService().listValues("Agency/Competition", Arrays.asList(agency));
-  if(competitions.contains(competition)){
-     createCompetition = false;
-     List<string> titles = getCategoryService().listValues("Agency/Competition", Arrays.asList(agency, competition));
-     createTitle = !titles.contains(title);
+  //set defaults of whether to add new entry or not to "true"
+  Boolean createAgency = true;
+  Boolean createCompetition = true;
+  Boolean createTitle = true;
+
+  //retrieve the categories we will want to check to see if need new entries
+  List<String> items = getCategoryService().listValues("Agency/Competition");
+  List<String> agencyOnly = getCategoryService().listValues("Agency");
+
+  //-----------------------------------------------------------
+  //Loop through Agency/Competition and see if new entry needed
+  //-----------------------------------------------------------
+
+  if(items.contains(agency)){
+    createAgency = false;
+    List<string> competitions = getCategoryService().listValues("Agency/Competition", Arrays.asList(agency));
+    if(competitions.contains(competition)){
+       createCompetition = false;
+       List<string> titles = getCategoryService().listValues("Agency/Competition", Arrays.asList(agency, competition));
+       createTitle = !titles.contains(title);
+    }
   }
-}
 
-if(createAgency){
-   getCategoryService().createCategoryValue("Agency/Competition", agency, "");
-}
-if(createCompetition){
-   getCategoryService().createCategoryValue("Agency/Competition", Arrays.asList(agency), competition, "");
-}
-if(createTitle){
-   getCategoryService().createCategoryValue("Agency/Competition", Arrays.asList(agency, competition), title, "");
-}
+  //-----------------------------------------------------
+  // Create any additional Agency/Competition entries needed
+  //-----------------------------------------------------
+  if(createAgency){
+     getCategoryService().createCategoryValue("Agency/Competition", agency, "");
+  }
+  if(createCompetition){
+     getCategoryService().createCategoryValue("Agency/Competition", Arrays.asList(agency), competition, "");
+  }
+  if(createTitle){
+     getCategoryService().createCategoryValue("Agency/Competition", Arrays.asList(agency, competition), title, "");
+  }
 
-if(!agencyOnly.contains(agency)){
- getCategoryService().createCategoryValue("Agency", agency, "");
-}
+  // -------------------------------------------------------
+  // Check if entry needed for Agency
+  //--------------------------------------------------------
+  if(!agencyOnly.contains(agency)){
+   getCategoryService().createCategoryValue("Agency", agency, "");
+  }
 
-Document grant = getDocumentService().lockDocument(document);
-grant.setHierarchicalCategory("Agency/Competition", Arrays.asList(agency, competition, title));
+
+  //--------------------------------------------------------------
+  //Assign the Agency/Compeition entry to Grant
+  //-------------------------------------------------------------
+  Document grant = getDocumentService().lockDocument(document);
+  grant.setHierarchicalCategory("Agency/Competition", Arrays.asList(agency, competition, title));
+
 
 }
